eba7b64c15e3d2b5ecc8a96a428b51d1977c137a
tomciv9000 <eugene.white@gmail.com>
2020-01-07 19:10:15 -0500

    Add approved column to users table

--- db/migrate/20200107233411_create_users.rb
+++ db/migrate/20200107233411_create_users.rb
@@ -5,6 +5,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.string :first_name
       t.string :last_name
       t.string :email
+      t.boolean :approved, :default => false
       t.integer :account_id
 
       t.timestamps
