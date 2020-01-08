7c28a0c8b5e94e788779b3f50c309fea4a422768
tomciv9000 <eugene.white@gmail.com>
2020-01-07 20:20:58 -0500

    Build sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  #root 'users#new'
+  
+  resources :users, only: [:new, :create, :show]
+  
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
