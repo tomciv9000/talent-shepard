e94f44bcedb2139747186e3ca1a1fd82f67fe3c7
tomciv9000 <eugene.white@gmail.com>
2020-01-08 04:09:33 -0500

    Nest users

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,16 @@
 class Account < ApplicationRecord
     has_many :users
     has_many :actors
+
+    accepts_nested_attributes_for :users
+
+
+  def users_attributes=(user_attributes)
+    user_attributes.values.each do |user_attribute|
+      if user_attribute["username"].present?
+        user = user.find_or_create_by(user_attribute)
+        self.users << user
+      end
+    end
+  end
 end
