72845d844db32d530020c9a643ff9b61bd4bd5dd
tomciv9000 <eugene.white@gmail.com>
2020-01-09 19:16:14 -0500

    Add under_contract boolean to reps

--- db/migrate/20200109230908_create_representations.rb
+++ db/migrate/20200109230908_create_representations.rb
@@ -1,7 +1,8 @@
 class CreateRepresentations < ActiveRecord::Migration[6.0]
   def change
     create_table :representations do |t|
-      t.signed :boolean
+      t.boolean :under_contract
+      t.text :notes
       t.integer :agency_id
       t.integer :actor_id
       t.timestamps
