0f4a2fb17fb78d8ea0c096c1adfba1828f89119c
tomciv9000 <eugene.white@gmail.com>
2020-01-14 13:31:14 -0500

    Fix signin errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < ApplicationController
+  skip_before_action :verified_user, only: [:new, :create]
   
   def new
   end
 
   def create
-    binding.pry
     @user = User.find_by(username: params[:username])
-      if @user &&  @user.authenticate(params[:user][:password])
+      if @user &&  @user.authenticate(params[:password])
         session[:user_id] = @user.id
         redirect_to user_path(@user)
       else
