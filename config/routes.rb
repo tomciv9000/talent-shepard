1be93872506f7a4b38696fea46d5dcc1d2543c8f
tomciv9000 <eugene.white@gmail.com>
2020-01-18 14:41:36 -0500

    Customize devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :admins, path: 'admins'
+  devise_for :admins, controllers: { sessions: 'admins/sessions', registrations: 'admins/registrations' }
   
-  devise_for :users, path: 'users'
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   get 'casting_offices/new'
   get 'casting_offices/show'
   get 'casting_offices/edit'
