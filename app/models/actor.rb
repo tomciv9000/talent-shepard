0d4eae8f28cbf58e0f90b7ef1c2f56463ed55b53
tomciv9000 <eugene.white@gmail.com>
2020-01-22 23:22:45 -0500

    Actor scope methods

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,14 +1,23 @@
 class Actor < ApplicationRecord
     belongs_to :agency
     acts_as_tenant(:agency)
+
     has_many :appointments
     has_many :casting_offices, through: :appointments
     has_many :bookings
     has_many :projects, through: :bookings
-    has_many :comments, as: :commentable
+    
+    scope :by_last_name, -> { order(last_name: :asc) }
+    scope :sag_aftra, -> { by_last_name.where(sag_aftra: true) }
+    scope :aea, -> { by_last_name.where(aea: true) }
+    scope :non_union, -> { by_last_name.where(aea: false, sag_aftra: false  ) }
+    
+    
 
     def full_name
         self.first_name + " " + self.last_name
     end
 
 end
+
+
--- app/models/project.rb
+++ app/models/project.rb
@@ -19,3 +19,4 @@ class Project < ApplicationRecord
 
 
 end
+
