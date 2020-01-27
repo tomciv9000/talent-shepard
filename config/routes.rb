563b81b205158f0bd3ddf6911071b17febc24633
tomciv9000 <eugene.white@gmail.com>
2020-01-27 17:15:15 -0500

    Format and clean-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
   devise_for :users, controllers: {registrations: 'users/registrations', sessions: 'users/sessions', omniauth_callbacks: 'users/omniauth_callbacks' }
+  
   devise_scope :user do
     get "/users/sign_out" => 'users/sessions#destroy'
     get '/users/registrations/agency_for_facebook', to: 'users/registrations#agency_for_facebook', as: 'agency_for_facebook'
@@ -34,6 +35,13 @@ Rails.application.routes.draw do
     resources :appointments
   end
   
+  
+  
+  resources :casting_offices
+  resources :bookings
+  resources :users
+  resources :appointments, only: [:index, :new, :create, :show, :edit]
+  
   get '/projects/:id/confirm_delete', to: 'projects#confirm_delete', as: 'confirm_project_delete'
   get '/actors/:id/confirm_delete', to: 'actors#confirm_delete', as: 'confirm_actor_delete'
   get '/appointments/:id/confirm_delete', to: 'appointments#confirm_delete', as: 'confirm_appointment_delete'
@@ -41,13 +49,6 @@ Rails.application.routes.draw do
   get '/bookings/:id/confirm_delete', to: 'bookings#confirm_delete', as: 'confirm_booking_delete'
   get '/users/:id/confirm_delete', to: 'users#confirm_delete', as: 'confirm_user_delete'
   get '/users/:id/permissions', to: 'users#permissions', as: 'edit_user_permissions'
-  #get '/users/registrations/agency_for_facebook', to: 'users/registrations#agency_for_facebook', as: 'agency_for_facebook'
-  #get '/users/registrations/facebook_new', to: 'users/registrations#facebook_new', as: 'facebook_user_registration' 
-  resources :casting_offices
-  resources :bookings
-  resources :users
-  resources :appointments, only: [:index, :new, :create, :show, :edit]
-  
   
 
 end
