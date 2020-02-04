af92d4f65c2340409d7355c5de84413d65da2dd8
tomciv9000 <eugene.white@gmail.com>
2020-02-03 23:40:32 -0500

    Remove comments

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,4 @@
+#after rails is executed, gemfile is loaded, bundle, bootstrap as well
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
