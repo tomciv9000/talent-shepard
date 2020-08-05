e30aa565f6e857dfe43ad97aa4a050d4e740cff5
tomciv9000 <eugene.white@gmail.com>
2020-08-05 13:35:44 -0400

    Adjust whitespace

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,12 +1,10 @@
 class Users::SessionsController < Devise::SessionsController
-   before_action :configure_sign_in_params, only: [:create]
-
+  before_action :configure_sign_in_params, only: [:create]
 
   protected
 
   def configure_sign_in_params
     devise_parameter_sanitizer.permit(:sign_in, keys: [:first_name, :last_name, :agency_id])
   end
-
   
 end
