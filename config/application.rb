adcf2e439174ba9887d5dc3383e19ae1b37f337f
tomciv9000 <eugene.white@gmail.com>
2020-08-20 14:59:34 -0400

    Update config autoloader to classic

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ module TalentShepherd
   class Application < Rails::Application
     
     config.load_defaults 6.0
+    config.autoloader = :classic
     config.autoload_paths += %W(#{config.root}/lib)
 
   end
